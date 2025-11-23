f1b5a1489e2074d52ff3b505145f904dbd1a8c60
Haytem Salim <salimhaytem.11@gmail.com>
2025-11-23 23:42:49 +0100

    third version

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,7 +7,91 @@ const PortfolioProjects = () => {
   // Données des projets
   const allProjects = [
     {
-      id: 1,
+  id: 11,
+  category: "Web App",
+  title: "HR Manager Pro – Plateforme de Ressource Humaine",
+  description:
+    "HRMaster est une application web professionnelle de gestion des ressources humaines incluant la gestion des employés, départements, demandes de congés avec validation, suivi de présence, fiches de paie, dashboards RH et employé, ainsi qu’un chatbot intelligent basé sur Ollama.",
+  image: "p14.jpg",
+  tags: ["Django", "JavaScript" ,"Tailwind CSS", "MySQL", "XAMPP", "Ollama"],
+  link: "#",
+},
+{
+    id: 7,
+    category: "Web app ",
+    title: "EduMatch – Plateforme IA de mise en relation étudiant-tuteur",
+    description:
+      "EduMatch est une plateforme web permettant aux étudiants de trouver des tuteurs en fonction de leurs matières, disponibilités et évaluations, avec un système de chat en temps réel et un calendrier intégré.",
+    image: "p7.jpg",
+    tags: ["Next.js", "Node.js", "Express.js", "Tailwind", "MongoDB"],
+    link: "#",
+  },
+  {
+  id: 1,
+  category: "web app ",
+  title: "CharityPlus – Plateforme de Don Caritatif en Ligne",
+  description:
+    "CharityPlus est une solution web complète de collecte de dons avec paiements sécurisés via Stripe, gestion et suivi des donateurs, gamification (badges et points de contribution), tableau de bord d’analyse avancée et interface moderne pour faciliter l’engagement caritatif.",
+  image: "p10.jpg",
+  tags: ["Spring Boot", "Stripe", "JPA", "Thymeleaf", "Bootstrap"],
+  link: "#",
+},
+ {
+  id: 11,
+  category: "Data",
+  title: "Fertilizer ETL – Dashboard Interactif",
+  description:
+    "Projet ETL pour l'analyse des données de fertilisants : extraction depuis CSV, transformation (coûts par tonne, coûts totaux) et visualisation via un dashboard interactif avec filtres temps réel et export CSV.",
+  image: "p11.jpg",
+  tags: ["Python", "Flask", "Pandas", "Chart.js", "Tailwind CSS", "Streamlit", "CSV", "ETL"],
+  link: "#",
+},
+{
+  id: 12,
+  category: "AI / Machine Learning",
+  title: "Diabetes Detector – Machine Learning Prediction App",
+  description:
+    "Application d'analyse médicale basée sur le Machine Learning permettant de prédire le risque de diabète à partir de données cliniques. Entraînée sur un dataset Kaggle avec visualisations, preprocessing avancé et évaluation du modèle.",
+  image: "p12.jpg",
+  tags: ["Python", "Scikit-Learn", "Pandas", "Flask", "Machine Learning", "Kaggle"],
+  link: "#",
+},
+{
+  id: 13,
+  category: "AI / Machine Learning",
+  title: "Heart Disease Prediction – Advanced ML Analysis & Web App",
+  description:
+    "Application de prédiction de maladies cardiaques basée sur plusieurs modèles de Machine Learning, avec preprocessing avancé, analyse exploratoire, comparaison des performances et interfaces web Streamlit & Flask.",
+  image: "p13.jpg",
+  tags: ["Python", "Scikit-Learn", "Flask", "Streamlit", "Pandas", "Machine Learning", "EDA"],
+  link: "#",
+},
+{
+  "id": 13,
+  "category": "AI / Chatbot",
+  "title": "RAG Chatbot – Assistant de Connaissances Local avec Ollama",
+  "description": "Ce projet implémente un chatbot basé sur l'architecture RAG (Retrieval-Augmented Generation) qui permet de répondre à des questions en se basant sur des documents locaux.",
+  "image": "p16.jpg",
+  "tags": [
+    "Python",
+    "LangChain",
+    "Ollama",
+    "Chroma",
+  ],
+  "link": "#"
+},
+{
+  id: 12,
+  category: "Mobile App",
+  title: "Smart Presence – Application Mobile de Gestion de Présence",
+  description:
+    "Smart Presence est une application mobile complète développée pour moderniser et automatiser le suivi des présences dans les établissements scolaires. ",
+  image: "p15.jpg",
+  tags: [
+    "Java", "XML UI","SQLite","Firebase","Google Maps API","Gemini AI",],
+  link: "#",
+},{
+      id: 10,
       category: "AI",
       title: "GigBooster – AI-Powered Fiverr Gig SEO Generator",
       description:
@@ -66,6 +150,12 @@ const PortfolioProjects = () => {
       tags: ["React", "Bootstrap"],
       link: "#",
     },
+
+
+
+
+
+  
   ];
 
   const displayedProjects = showAllProjects ? allProjects : allProjects.slice(0, 4);
